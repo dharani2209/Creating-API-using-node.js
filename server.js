5b29269a573c8456139a96424070f3f9af960eeb
dharani2209 <dharani.c@tringapps.com>
2021-12-10 18:09:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+  res.send("Hi!..This is my page");
+});
+const tourismRoutes = require('./src/routes/tourism.routes')
+app.use('/api/v1/tourism', tourismRoutes)
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
